a89e3b5cb95e8bd87a52ee46e413d6dba48d2a6e
zhuy-nguyenz <graio911@gmail.com>
2021-07-06 08:30:21 +0700

    1st

---
+++ src/MathCalculator.java
@@ -0,0 +1,17 @@
+public class MathCalculator implements Calculator {
+    public double add(double first, double second) {
+        return first + second;
+    }
+
+    public double sub(double first, double second) {
+        return first - second;
+    }
+
+    public double mul(double first, double second) {
+        return first * second;
+    }
+
+    public double div(double first, double second) {
+        return first / second;
+    }
+}
